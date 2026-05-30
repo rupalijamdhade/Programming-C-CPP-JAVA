296833a56cd2ad8e152c01778315b6e6087555e7
rupalijamdhade <rupalibirhade99@gmail.com>
2026-05-30 23:56:05 +0530

    Add files via upload

---
+++ C_programing/competitive_programs/problem2.c
@@ -0,0 +1,16 @@
+//print marvellous 5 times
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("marvellous\n");
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
